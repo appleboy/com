4b08341e949a86773cfb01c32e871c078264c555
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-05-17 11:02:35 +0800

    chore(convert): add str to byte and byte to str (#3)

--- convert/strings.go
+++ convert/strings.go
@@ -3,6 +3,8 @@ package convert
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"reflect"
+	"unsafe"
 )
 
 // MD5Hash for md5 hash string
@@ -10,4 +12,29 @@ func MD5Hash(text string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(text))
 	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+// BytesToStr converts byte slice to a string without memory allocation.
+// See https://groups.google.com/forum/#!msg/Golang-Nuts/ENgbUzYvCuU/90yGx7GUAgAJ .
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func BytesToStr(b []byte) string {
+	/* #nosec G103 */
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// StrToBytes converts string to a byte slice without memory allocation.
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func StrToBytes(s string) (b []byte) {
+	/* #nosec G103 */
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	/* #nosec G103 */
+	sh := *(*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Len = sh.Len
+	bh.Cap = sh.Len
+	return b
 }
