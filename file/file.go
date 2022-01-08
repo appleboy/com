5d9758492aae58e64ec03452f9e2b1bf47b9535a
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-01-08 11:28:57 +0800

    chore(file): add copy
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- file/file.go
+++ file/file.go
@@ -1,6 +1,8 @@
 package file
 
 import (
+	"fmt"
+	"io"
 	"os"
 )
 
@@ -34,4 +36,57 @@ func Remove(filePath string) error {
 	}
 
 	return os.Remove(filePath)
+}
+
+var BUFFERSIZE int64
+
+// Copy files
+// See the ref:
+// https://opensource.com/article/18/6/copying-files-go
+func Copy(src, dst string, BUFFERSIZE int64) error {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file.", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	_, err = os.Stat(dst)
+	if err == nil {
+		return fmt.Errorf("File %s already exists.", dst)
+	}
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, BUFFERSIZE)
+	for {
+		n, err := source.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+
+		if _, err := destination.Write(buf[:n]); err != nil {
+			return err
+		}
+	}
+	return err
 }
