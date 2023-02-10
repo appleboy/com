e163c1556e73fdb2076114e738b00ddad4d653d0
Bo-Yi.Wu <appleboy.tw@gmail.com>
2023-02-10 19:56:34 +0800

    chore: upgrade
    Signed-off-by: Bo-Yi.Wu <appleboy.tw@gmail.com>

--- convert/strings_1.19.go
+++ convert/strings_1.19.go
@@ -3,6 +3,8 @@
 
 package convert
 
+import "unsafe"
+
 // BytesToStr converts byte slice to a string without memory allocation.
 // See https://groups.google.com/forum/#!msg/Golang-Nuts/ENgbUzYvCuU/90yGx7GUAgAJ .
 //
